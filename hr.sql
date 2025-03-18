57aa57c5384617d0d94aaf0bc3eb9916e72fc1d9
kims0711 <altjd10@gmail.com>
2025-03-18 18:01:50 +0900

    오라클 예제 (is null 까지)

---
+++ hr.sql
@@ -0,0 +1,160 @@
+-- employees (scott의 emp 동일개념)
+-- first_name, last_name, job_id 조회
+SELECT e.FIRST_NAME, e.LAST_NAME, e.JOB_ID FROM EMPLOYEES e; 
+
+-- job_id 중복 제외 후 출력
+SELECT DISTINCT  e.JOB_ID FROM EMPLOYEES e;  --DISTINCT : 중복 제외(2개 이상 요소 조회 할 때는 두 요소가 완전히 같아야 중복)
+
+--사번이 176인 사원의 last_name과 부서번호 조회하기
+SELECT
+	e.LAST_NAME,
+	e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.EMPLOYEE_ID = 176;
+--급여가 12000이상되는 사원의 last_name과 급여 조회하기
+SELECT
+	e.LAST_NAME,
+	e.SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	e.SALARY >= 12000;
+
+--급여가 5000~12000범위가 아닌 사원의 last_name과 급여 조회하기 
+SELECT
+	e.LAST_NAME,
+	e.SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	NOT (e.SALARY >= 5000
+		AND e.SALARY <= 12000); 
+
+--위에거 BETWEEN으로 가능
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.SALARY NOT BETWEEN 5000 AND 12000;
+
+--IN
+--20,50번 부서에 근무하는 사원 조회(LAST_NAME, 부서번호,), 그리고 LAST_NAME 오름차순
+SELECT
+	e.LAST_NAME,
+	e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.DEPARTMENT_ID IN(20, 50)
+ORDER BY
+	E.LAST_NAME ASC;
+--급여가 2500,3500,7000이 아니고 직무가 SA_REP, ST_CLERK가 아닌 사원 조회
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.SALARY NOT IN(2500, 3500, 7000)
+	AND e.JOB_ID NOT IN('SA_REP', 'ST_CLERK');
+
+--날짜 데이터도 '' 사용
+--고용일이 2014년인 사원 조회(2014-01-01 ~ 2014-12-31)
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.HIRE_DATE BETWEEN '2014-01-01' AND '2014-12-31';
+
+--LIKE
+-- ex1)LAST_NAME에 U가 포함되는 사원들의 사번, LAST_NAME 조회
+SELECT
+	e.EMPLOYEE_ID,
+	e.LAST_NAME
+FROM
+	EMPLOYEES e
+WHERE
+	e.LAST_NAME LIKE '%U%';
+-- ex2)LAST_NAME의 4번째 글자가 a인 사원들의 사번 LAST_NAME 조회
+SELECT
+	e.EMPLOYEE_ID,
+	e.LAST_NAME
+FROM
+	EMPLOYEES e
+WHERE
+	e.LAST_NAME LIKE '___a%';
+-- ex3)LAST_NAME에 a 혹은 e가 있는 사람들의 사번, LAST_NAME조회 (LAST_NAME내림차순)
+SELECT
+	e.EMPLOYEE_ID,
+	e.LAST_NAME
+FROM
+	EMPLOYEES e
+WHERE
+	e.LAST_NAME LIKE '%a%'
+	OR e.LAST_NAME LIKE '%e%'
+ORDER BY
+	e.LAST_NAME DESC ;
+-- ex4)LAST_NAME에 a와 e가 모두 있는 사원의 사번, LAST_NAME조회(LAST_NAME내림차순)
+SELECT
+	e.EMPLOYEE_ID,
+	e.LAST_NAME
+FROM
+	EMPLOYEES e
+WHERE
+	e.LAST_NAME LIKE '%a%e%'
+	or e.LAST_NAME LIKE '%e%a%'
+ORDER BY
+	e.LAST_NAME DESC;
+
+--IS NULL
+-- ex1) 매니저가 없는 사원들의 LAST_NAME, JOB_ID 조회
+SELECT
+	e.LAST_NAME,
+	e.JOB_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.MANAGER_ID IS NULL;
+
+-- ex2) ST_CLERK인 직업을 가진 사원이 없는 부서 번호 조회(단, 부서번호가 널 값인 부서 제외)
+SELECT
+	DISTINCT e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.JOB_ID != 'ST_CLERK' -- NOT e.JOB_ID = 'ST_CLERK' 으로도 가능
+	AND e.DEPARTMENT_ID IS NOT NULL
+ORDER BY
+	e.DEPARTMENT_ID; --중복 제외랑 정렬은 내가 그냥 한 것임
+	
+-- ex3) COMMISSION_PCT가 NULL이 아닌 사원들 중에서 COMMISSION=SALARY * COMMISSION_PCT를 구한다
+--      계산결과와 함께 사번, FIRST_NAME, JOB_ID를 출력
+SELECT
+	e.SALARY * e.COMMISSION_PCT AS COMMISSION,
+	e.EMPLOYEE_ID,
+	e.FIRST_NAME,
+	e.JOB_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.COMMISSION_PCT IS NOT NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- scott.sql
+++ scott.sql
@@ -17,3 +17,260 @@ ALTER USER scott IDENTIFIED BY tiger;
 -- 계정 잠김 해제
 ALTER USER scott account unlock;
 
+--중복 제외 : select distinct
+
+-- 별칭 만들기
+SELECT ename, sal, sal*12+comm AS "annsal",comm FROM EMP; --AS는 선택
+
+SELECT ename, sal, sal*12+comm AS annsal, comm   FROM EMP;
+
+SELECT ename, sal, sal*12+comm,comm AS 연봉 FROM EMP;
+
+SELECT ename, sal, sal*12+comm,comm AS "연 봉" FROM EMP; --이름에 공백에 있으면 쌍따옴표 필수이다!
+
+SELECT ename 사원명, sal 급여, sal*12+comm "연 봉",comm 수당 FROM EMP e; --여기서 EMP e의 e도 별칭이다(필수는 아님)
+
+--원하는 순서대로 출력 데이터를 정렬하여 조회하기
+--emp 테이블의 모든 열을 급여 기준으로 오름차순 조회 
+SELECT * FROM EMP e ORDER BY e.SAL ASC; -- ASC : 오름차순 -> 근데 default가 오름차순이라 ASC는 생략 가능하다!
+SELECT * FROM EMP e ORDER BY e.SAL DESC; -- DESC: 내림차순 
+SELECT * FROM emp e ORDER BY e.sal; -- default가 오름차순이라서 얘도 ASC랑 똑같이 나옴
+
+--사번,이름,직무만 급여기준으로 내림차순 조회
+SELECT e.DEPTNO, e.ENAME, e.JOB FROM EMP e ORDER  BY e.SAL DESC;
+SELECT e.DEPTNO, e.ENAME, e.JOB, e.SAL FROM EMP e ORDER  BY e.SAL DESC; --그냥 급여도 출력해서 확인해봄
+
+-- 부서번호의 오름차순, 급여의 내림차순
+SELECT * FROM EMP e ORDER BY e.DEPTNO ASC, e.SAL DESC;
+
+-- 별칭 바꾸고 조건에 맞춰서 정렬 후 조회하기
+SELECT
+	e.EMPNO AS EMPLOYEE_NO,
+	e.ENAME AS EMPLOYEE_NAME,
+	e.MGR AS MANAGER,
+	e.sal AS SALARY,
+	e.COMM AS COMMISION,
+	e.DEPTNO AS DEPARTMENT_NO
+FROM
+	emp e
+ORDER BY
+	e.DEPTNO DESC,
+	e.ENAME ASC;
+
+-- where : 조회 시 조건 부여
+-- 부서번호가 30번인 사원 조회 
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 30;
+
+--사원번호가 7782인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.EMPNO = 7782;
+
+--부서번호가 30이고 직책이 sales man인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.DEPTNO=30
+	AND e.JOB = 'SALESMAN'; --오라클에서 문자열은 ''임 !! (그리고 문자열은 정확히 대소문자 구분 해줘야 함)
+	
+--사원번호가 7499이고 부서번호가 30번인 사원 조회 -> AND
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.EMPNO = 7499
+	AND e.DEPTNO = 30;
+
+--사원번호가 7499이거나 부서번호가 30번인 사원 조회 -> OR
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.EMPNO = 7499
+	OR e.DEPTNO = 30;
+
+
+--where절에서 쓸 수 있는 연산자
+-- 1)산술연산자: + - * /
+-- 2)비교연산자: >, <, >=, <=
+-- 3)등가비교연산자: 같다 =, 같지않다 (!=, <>, ^=)
+-- 4)논리부정연산자: NOT
+-- 5)		   : IN 
+-- 6)범위연산자: BETWEEN A AND B
+-- 7)검색: LIKE 연산자와 와일드카드(_ , %)
+-- 8)IS NULL : 널과 같다 (자바에서의 ==null과 같은 뜻)
+
+
+--연봉이 36000인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.SAL * 12 = 36000;
+
+--급여가 3000이상인 사원
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.sal >= 3000;
+
+--급여가 2500 이상이고 직업이 ANALYST 인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.SAL >= 2500
+	AND e.JOB = 'ANALYST';
+
+--문자를 대소비교 할 수 있음
+--사원명의 첫 문자가 F와 같거나 F보다 뒤에 있는 사원 조회 (코드값 때문에)
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.ename >= 'F';
+
+--급여가 3000이 아닌 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.sal != 3000; --(<>랑 ^=도 가능은 함 근데 !=이 익숙하니까)
+	
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	NOT e.sal = 3000; -- 이렇게 not 이용해서도 가능
+	
+
+--In 연산자
+-- job이 MANAGER이거나 SALESMAN이거나 CLERK인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.job = 'MANAGER'
+	OR e.JOB = 'SALESMAN'
+	OR e.JOB = 'CLERK'; --원래 하던 방법
+
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.job IN ('MANAGER', 'SALESMAN', 'CLERK'); -- IN 활용하여 좀 더 간단하게
+	
+--위 세 직업이 아닌 사원 조회하기
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.job NOT IN ('MANAGER', 'SALESMAN', 'CLERK'); --그냥 NOT 앞에 붙이면 됨
+	
+--BETWEEN A AND B
+--급여가 2000이상 3000이하
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.SAL BETWEEN 2000 AND 3000; --이렇게 하면 됨
+	
+--NOT BETWEEN도 가능
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.SAL NOT BETWEEN 2000 AND 3000; --2000미만 3000초과 
+
+--LIKE연산자 
+--_ : 어떤 값이든 상관없이 한 개의 문자열 데이터를 의미
+--% : 길이와 상관없이(문자 없는 경우도 포함) 모든 문자열 데이터를 의미
+
+	
+--ex1) 사원명이 S로 시작하는 사원을 조회하고 싶음
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.ENAME LIKE 'S%';
+
+--ex2) 사원명의 두번째 글자가 L인 사원 조회
+SELECT  *
+FROM emp e
+WHERE e.ENAME LIKE '_L%'; --이렇게 하면 됨
+
+--ex3) 사원명에 AM이 포함된 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.ENAME LIKE '%AM%';
+
+--ex4) 사원명에 AM이 포함되지 않은 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.ENAME NOT LIKE '%AM%';
+
+--IS NULL
+--COMM이 NULL인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.COMM IS NULL;
+
+--MGR이 NULL인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.MGR IS NULL; --mgr: 직속상관
+	
+--직속상관이 있는 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.MGR IS NOT NULL; -- NOT e.mgr IS NULL 도 가능했음
+
+
+
+
+
+
+
+
+
+
